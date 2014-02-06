a46e0d8585537c7e5a008846e4ace693055bbaa1
Richard Rivera <rrivera@esri.com>
2014-02-06 10:59:18 -0800

    updated paths to geoprocessing

--- data/geoprocessing/source/Download/DiscoveryViewerImageryDownloader.py
+++ data/geoprocessing/source/Download/DiscoveryViewerImageryDownloader.py
@@ -4,32 +4,20 @@ import arcpy, json, urllib2, shutil, os, datetime
 from zipfile import ZipFile
 
 
-def getDirectoryList(folder):
-    directoryList = []
-    for root, subFolders, files in os.walk(folder):
-        for subFolder in subFolders:
-            directoryList.append(subFolder)
-    return directoryList
-
-
 def zipDirectoryToScratch(folder):
-    rootChildDirectories = getDirectoryList(folder)
     zipName = "ImageryExport" + datetime.date.today().strftime('%m_%d_%Y') + ".zip"
     zipPath = os.path.join(arcpy.env.scratchFolder, zipName)
-    with ZipFile(zipPath, "w") as zipFile:
-        for rootChildDirectory in rootChildDirectories:
-            childDirectoryFullPath = os.path.join(folder, rootChildDirectory)
-            addDirectoryToZip(childDirectoryFullPath, rootChildDirectory, zipFile)
-        zipFile.close()
+    root_len = len(os.path.abspath(folder))
+    with ZipFile(zipPath, "w") as zip:
+        for root, dir, files in os.walk(folder):
+            archive_root = os.path.abspath(root)[root_len:]
+            arcpy.AddMessage("archive_root: {0}".format(archive_root))
+            for f in files:
+                fullpath = os.path.join(root, f)
+                archive_name = os.path.join(archive_root, f)
+                arcpy.AddMessage("archive_name: {0}".format(archive_name))
+                zip.write(fullpath, archive_name)
     return zipPath
-
-
-def addDirectoryToZip(fullPath, relativePath, zipFile):
-    for root, dirs, files in os.walk(fullPath):
-        for currentFile in files:
-            writePath = os.path.join(relativePath, currentFile)
-            zipFile.write(os.path.join(root, currentFile), writePath)
-
 
 def download(url, outFile):
     r = urllib2.urlopen(urllib2.Request(url))
@@ -44,32 +32,36 @@ def downloadItemsFromJson(urlJson, rootOutFolder):
     downloadArray = json.loads(urlJson)
     for downloadItem in downloadArray:
         try:
+            id = downloadItem["id"]
             url = downloadItem["url"]
             label = downloadItem["label"]
             serviceName = downloadItem["serviceName"]
             outFolder = os.path.join(rootOutFolder, serviceName)
+            folderParts = id.split("\\")
+            for part in folderParts:
+                if part != "." and part != label:
+                    arcpy.AddMessage("adding url part: " + part)
+                    outFolder = os.path.join(outFolder, part)
+                    arcpy.AddMessage("folder: " + outFolder)
             if not os.path.exists(outFolder):
                 os.makedirs(outFolder)
             outFile = os.path.join(outFolder, label)
             arcpy.AddMessage("Downloading %s" % url)
+            arcpy.AddMessage("Writing File: " + outFile)
             download(url, outFile)
         except:
             arcpy.AddMessage("Invalid download item passed. continuing")
 
 
 def cleanup(folder):
-    #removes all directories in the scratch folder
-    childDirectories = getDirectoryList(folder)
-    for childDirectory in childDirectories:
-        childDirectoryFullPath = os.path.join(folder, childDirectory)
-        shutil.rmtree(childDirectoryFullPath)
+    arcpy.AddMessage("Cleanup:" + folder)
+    shutil.rmtree(folder)
 
 
 inputJson = arcpy.GetParameterAsText(0)
 outFolder = os.path.join(arcpy.env.scratchFolder, "downloadItems")
 if not os.path.exists(outFolder):
     os.makedirs(outFolder)
-# outFolder = "C:\\temp"
 downloadItemsFromJson(inputJson, outFolder)
 zipPath = zipDirectoryToScratch(outFolder)
 cleanup(outFolder)
