4d751e0248af1bda983ac4696053afab115a6a12
Richard Rivera <rrivera@esri.com>
2014-09-02 12:06:57 -0700

    multi metadata search

--- site/js/imagediscovery/ui/discover/base/SearchByMetadata.js
+++ site/js/imagediscovery/ui/discover/base/SearchByMetadata.js
@@ -3,9 +3,12 @@ define([
         "dojo/text!./templates/SearchByMetadataTemplate.html",
         "./BaseSearchByWidget",
         "dijit/form/FilteringSelect",
+        "dijit/form/RadioButton",
         "esriviewer/base/DataLoaderSupport",
         "dojo/store/Memory",
         "dojo/topic",
+        "dojo/dom-construct",
+        "dojo/dom-style",
         "dojo/_base/lang",
         "esri/tasks/QueryTask" ,
         "esri/tasks/query",
@@ -21,7 +24,7 @@ define([
         "dojo/dom-attr"
 
     ],
-    function (declare, template, BaseSearchByWidget, FilteringSelect, DataLoaderSupport, Memory, topic, lang, QueryTask, Query, SimpleFillSymbol, SimpleMarkerSymbol, SimpleLineSymbol, Color, Graphic, Point, Polygon, Polyline, Extent,domAttr) {
+    function (declare, template, BaseSearchByWidget, FilteringSelect, RadioButton, DataLoaderSupport, Memory, topic, domConstruct, domStyle, lang, QueryTask, Query, SimpleFillSymbol, SimpleMarkerSymbol, SimpleLineSymbol, Color, Graphic, Point, Polygon, Polyline, Extent, domAttr) {
         return declare(
             [ BaseSearchByWidget, DataLoaderSupport],
             {
@@ -40,12 +43,10 @@ define([
                     var metadataConfig;
                     topic.publish(IMAGERY_GLOBALS.EVENTS.CONFIGURATION.GET_ENTRY, "imageQueryMetadataConfiguration", function (metadataConf) {
                         metadataConfig = metadataConf;
-
-
                     });
                     if (metadataConfig) {
-                        if(metadataConfig.metadataDetailsLabel){
-                           domAttr.set(this.metadataDetailsLabel,"innerHTML",metadataConfig.metadataDetailsLabel);
+                        if (metadataConfig.metadataDetailsLabel) {
+                            domAttr.set(this.metadataDetailsLabel, "innerHTML", metadataConfig.metadataDetailsLabel);
                         }
                         if (metadataConfig.url) {
                             this.loadJsonP(metadataConfig.url, {f: "json"}, lang.hitch(this, this.handleMetadataConfigLoaded));
@@ -53,23 +54,97 @@ define([
                     }
                 },
                 handleMetadataConfigLoaded: function (config) {
-                    this.queryField = config.queryField;
-                    this.metadataSearchServiceConfig = config.searchService;
-                    this.metadataSelect = new FilteringSelect({
-                        style: {
-                            width: "85%"
-                        },
-                        store: new Memory({
-                            idProperty: "name",
-                            label: "name",
-                            data: config.data
+                    if (!config || !config.metadataLayers || !config.metadataLayers.length) {
+                        return;
+                    }
+                    var i;
+                    this.metadataSearchObjects = {};
+                    var newMetadataSearchObject;
+                    var key;
+                    var count = 0;
+                    for (i = 0; i < config.metadataLayers.length; i++) {
+                        newMetadataSearchObject = this.createMetadataSearchObject(config.metadataLayers[i]);
+                        if (newMetadataSearchObject) {
+                            this.metadataSearchObjects[newMetadataSearchObject._id] = newMetadataSearchObject;
+                            count++;
+                        }
+                    }
+                    if (count === 1) {
+                        this.currentMetadataSearchObject = newMetadataSearchObject;
+                        this.currentMetadataSearchObject.metadataSelect.placeAt(this.metadataSelectFilter);
+                        this.currentMetadataSearchObject.metadataSelect.startup();
+
+                    }
+                    else {
+                        var currentMetadataSearchObject;
+                        var currentMetadataRadioButton;
+                        var currentSelectFilterContainer;
+                        var checked = true;
+                        for (key in this.metadataSearchObjects) {
+                            if (this.metadataSearchObjects.hasOwnProperty(key)) {
+                                currentMetadataSearchObject = this.metadataSearchObjects[key];
+                                currentMetadataRadioButton = new RadioButton({
+                                    checked: checked,
+                                    value: key,
+                                    name: "discoveryMetadataRadio"
+                                });
+                                currentMetadataRadioButton.on("click", lang.hitch(this, this.handleRadioSelectionChanged, key));
+                                currentMetadataSearchObject.searchFilterContainer = domConstruct.create("div");
+
+                                domConstruct.place(currentMetadataSearchObject.searchFilterContainer, this.metadataSelectFilter);
+                                currentMetadataSearchObject.metadataSelect.placeAt(currentMetadataSearchObject.searchFilterContainer);
+                                if (!checked) {
+                                    domStyle.set(currentMetadataSearchObject.searchFilterContainer, "display", "none");
+                                }
+                                else {
+                                    this.currentMetadataSearchObject = currentMetadataSearchObject;
+                                }
+                                checked = false;
+                                currentMetadataRadioButton.placeAt(this.metadataRadioButtonsContainer);
+                                domConstruct.place(domConstruct.create("span", {innerHTML: currentMetadataSearchObject.label, style: {marginRight: "5px"} }), this.metadataRadioButtonsContainer);
+                            }
+                        }
+                    }
+                },
+                handleRadioSelectionChanged: function (searchObjectId) {
+                    console.log("handleRadioSelectionChanged");
+                    console.log(searchObjectId);
+                    if (this.currentMetadataSearchObject && this.currentMetadataSearchObject._id !== searchObjectId) {
+                        domStyle.set(this.currentMetadataSearchObject.searchFilterContainer, "display", "none");
+                    }
+                    if (this.currentMetadataSearchObject._id === searchObjectId) {
+                        return;
+                    }
+                    this.currentMetadataSearchObject = this.metadataSearchObjects[searchObjectId];
+                    if (this.currentMetadataSearchObject) {
+                        domStyle.set(this.currentMetadataSearchObject.searchFilterContainer, "display", "block");
+                    }
+
+
+                },
+                createMetadataSearchObject: function (searchLayer) {
+                    if (!searchLayer.queryField || !searchLayer.layerUrl || !searchLayer.data) {
+                        return;
+                    }
+                    return {
+                        _id: VIEWER_UTILS.generateUUID(),
+                        queryField: searchLayer.queryField,
+                        layerUrl: searchLayer.layerUrl,
+                        label: searchLayer.label || "Label",
+                        metadataSelect: new FilteringSelect({
+                            required: false,
+                            style: {
+                                width: "85%"
+                            },
+                            store: new Memory({
+                                idProperty: "name",
+                                label: "name",
+                                data: searchLayer.data
+                            })
                         })
-                    });
-                    this.metadataSelect.placeAt(this.metadataSelectFilter);
-                    this.metadataSelect.startup();
+                    };
                 },
                 initListeners: function () {
-
                     topic.subscribe(IMAGERY_GLOBALS.EVENTS.QUERY.RESULT.CLEAR, lang.hitch(this, this.handleResultsCleared));
                 },
                 handleResultsCleared: function () {
@@ -97,20 +172,23 @@ define([
                 },
 
                 searchMetadata: function () {
-                    var searchValue = this.metadataSelect.get("value");
+                    if (!this.currentMetadataSearchObject) {
+                        return;
+                    }
+                    var searchValue = this.currentMetadataSearchObject.metadataSelect.get("value");
                     if (!searchValue) {
                         return;
                     }
-                    if (!this.queryTask) {
-                        this.queryTask = new QueryTask(this.metadataSearchServiceConfig.url);
+                    if (!this.currentMetadataSearchObject.queryTask) {
+                        this.currentMetadataSearchObject.queryTask = new QueryTask(this.currentMetadataSearchObject.layerUrl);
 
                     }
                     var resultFeature, query = new Query();
-                    query.where = this.queryField + " = '" + searchValue + "'";
+                    query.where = this.currentMetadataSearchObject.queryField + " = '" + searchValue + "'";
                     query.outSpatialReference = this.map.extent.spatialReference;
                     console.log("Where clause: " + query.where);
                     query.returnGeometry = true;
-                    this.queryTask.execute(query, lang.hitch(this, function (results) {
+                    this.currentMetadataSearchObject.queryTask.execute(query, lang.hitch(this, function (results) {
                         if (results && results.features && results.features.length > 0) {
                             resultFeature = results.features[0];
                             if (resultFeature.geometry) {
